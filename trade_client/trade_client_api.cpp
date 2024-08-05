ae4c419e3bd89d5b417ee3b60dd5d3baeb30ee4b
SamuelDeng <mr.dengshuoling@gmail.com>
2024-08-05 19:47:29 +0800

    Update.

--- trade_client/trade_client_api.cpp
+++ trade_client/trade_client_api.cpp
@@ -1,17 +1,14 @@
-// OSX: g++ trade_client_api.cpp -o libtrade_client_api.dylib --std c++17 -shared -fPIC -L./ -ltrade_client 
+// OSX: g++ trade_client_api.cpp -o libtrade_client_api.dylib --std c++17 -shared -fPIC -L./ -ltrade_client
 // Linux: g++ trade_client_api.cpp -o libtrade_client_api.so --std c++17 -shared -fPIC -L./ -ltrade_client -Wl,-rpath ./
 
 #include "./trade_client.hpp"
 
-extern "C"
+extern "C" trade::TradeClient *get_trade_client_instance()
 {
-    trade::TradeClient *get_trade_client_instance()
-    {
-        static trade::TradeClient trade_client;
-        return &trade_client;
-    }
-
-    void start_trade_client(trade::callback_functype callback) { get_trade_client_instance()->Start(callback); }
-
-    void join_trade_client() { get_trade_client_instance()->Join(); }
+    static trade::TradeClient trade_client;
+    return &trade_client;
 }
+
+extern "C" void start_trade_client(trade::callback_functype callback) { get_trade_client_instance()->Start(callback); }
+
+extern "C" void join_trade_client() { get_trade_client_instance()->Join(); }
